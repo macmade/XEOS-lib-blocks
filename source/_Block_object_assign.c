05901a1d1086f87b1368806bda21ebc327953eee
macmade <macmade@xs-labs.com>
2015-02-24 01:46:48 +0100

    Added the new copyright notices.

--- source/_Block_object_assign.c
+++ source/_Block_object_assign.c
@@ -59,6 +59,11 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
+/*!
+ * @author          Jean-David Gadina
+ * @copyright       (c) 2010-2015, Jean-David Gadina - www.xs-labs.com
+ */
+
 #include <blocks.h>
 
 void _Block_object_assign( void * dest, void * source, int flags );
